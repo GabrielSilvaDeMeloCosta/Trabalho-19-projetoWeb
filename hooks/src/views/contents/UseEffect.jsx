010951cacf1ce734d0fc378421d16f96c10ded23
GabrielSilvaDeMeloCosta <gabrielcosta200504@gmail.com>
2021-12-09 16:25:42 +0000

    ProjetoEdnaldoPereiraImóveis

--- hooks/src/views/contents/UseEffect.jsx
+++ hooks/src/views/contents/UseEffect.jsx
@@ -1,6 +1,10 @@
 import './UseEffect.css'
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, Component } from "react"
+import ReactDOM from "react-dom";
 
+const Comprar = () =>{
+   
+}
 const UseEffect = props => {
 
     const [ notaFinal, setNotaFinal ] = useState(0)
@@ -39,7 +43,7 @@ const UseEffect = props => {
     function verificaSituacao(nota) {
         return nota >= 6 ? "Aprovado" : "Reprovado"
     }
-
+    
     return (
         <div>
             <div className="titulo">
@@ -48,7 +52,38 @@ const UseEffect = props => {
             </div>
 
             <div className="conteudo">
-            
+                <div className="caixa">
+                    <img src="https://res.listglobally.com/listings/5856296/76569403/5ceac5a3e19c299b5f8a780a8255c9c7?mode=max&w=1920"width="150px" height="150px"/>
+                    <h6>Cristal Lagoon</h6>
+                    <button type="button" class="button button1" onClick={ () =>alert("Preço: EUR 351.902 \nTelefone do Dono: 971 4(99)9999-9999")}>Comprar</button>
+                    
+                </div>
+                <div className="caixa">
+                    <img src="https://res.listglobally.com/listings/5874490/76534050/b27534fc648dde69dba264ec62f05e36?mode=max&w=1920"width="150px" height="150px"/>
+                    <h6>Unique Villa</h6>
+                    <button type="button" class="button button1" onClick={ () =>alert("Preço: EUR 8.039.863 \nTelefone do Dono: 971 4(99)9999-9999")}>Comprar</button>
+                </div>
+                <div className="caixa">
+                    <img src="https://res.listglobally.com/listings/5940067/75877269/b0adae7e4a71ce538a71c82590854bd2?mode=max&w=1920"width="150px" height="150px"/>
+                    <h6>Atlantic Villa</h6>
+                    <button type="button" class="button button1" onClick={ () =>alert("Preço: EUR 20.399.654 \nTelefone do Dono: 971 4(99)9999-9999")}>Comprar</button>
+                </div>
+                <br/>
+                <div className="caixa">
+                    <img src="https://res.listglobally.com/listings/5940067/75868544/4ec194a0087ea9582ecf73e859986d29?mode=max&w=1920"width="150px" height="150px"/>
+                    <h6>Moderna villa</h6>
+                    <button type="button" class="button button1" onClick={ () =>alert("Preço: EUR 479.992 \nTelefone do Dono: 971 4(99)9999-9999")}>Comprar</button>
+                </div>
+                <div className="caixa">
+                    <img src="https://res.listglobally.com/listings/5938533/75847954/dd215870821ab9e1b94a98d4d9368d92?mode=max&w=1920"width="150px" height="150px"/>
+                    <h6>Suíte Garden</h6>
+                    <button type="button" class="button button1" onClick={ () =>alert("Preço: EUR 1.225.587 \nTelefone do Dono: 971 4(99)9999-9999")}>Comprar</button>
+                </div>
+                <div className="caixa">
+                    <img src="https://res.listglobally.com/listings/5938296/75199922/1aa276dd37a753c3a5b84ea7913b9f66?mode=max&w=1920"width="150px" height="150px"/>
+                    <h6>Motor City</h6>
+                    <button type="button" class="button button1" onClick={ () =>alert("Preço: EUR 398.104 \nTelefone do Dono: 971 4(99)9999-9999")}>Comprar</button>
+                </div>
                 
             </div>
             <div className="footer">
